7b6a4a77477987d42a7ea16224c140f0047e789f
INSIDE\Dmitry.Mishutin <mishutindima@gmail.com>
2022-09-06 00:10:42 +0300

    Add Postgre support, create initial setting and migrations

--- InvestOneProject/InvestOne/settings.py
+++ InvestOneProject/InvestOne/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'InvestOne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "investonedb",
+        "USER":"postgres",
+        "PASSWORD":"Orion123",
+        "HOST":"localhost",
+        "PORT":"5432",
     }
 }
 
