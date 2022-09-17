c11f9912f7f1e932c3e4b97471099dfe4ce1f5c6
INSIDE\Dmitry.Mishutin <mishutindima@gmail.com>
2022-09-17 14:12:10 +0300

    add static files fix #6

--- InvestOneProject/InvestOne/settings.py
+++ InvestOneProject/InvestOne/settings.py
@@ -118,13 +118,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / "History/static"
+]
 STATIC_URL = 'static/'
+
+# Каталог, куда будут собраны все статические файлы
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "static" / "media"
